bc6e8ab4edc20892b8faffe4cf875fcdfcc5b048
vllaprilia <viololaaprilia@gmail.com>
2023-11-28 17:17:12 +0700

    file baru Baca File

---
+++ Tugas09-PetaniKodeFile/baca_file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    char buff[255];
+    FILE *fptr;
+
+    // membuka file
+    if ((fptr = fopen("puisi.txt", "r")) == NULL)
+    {
+        printf("Error: File tidak ada!");
+        // Tutup program karena file gak ada.
+        exit(1);
+    }
+
+    // baca isi file dengan gets lalu simpan ke buff
+    fgets(buff, 255, fptr);
+    // tampilkan isi file
+    printf("%s", buff);
+
+    // tutup file
+    fclose(fptr);
+}
